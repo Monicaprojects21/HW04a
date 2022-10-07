c44f4e2fac838f4eca18b3df1a7f1baf0d644cc3
Monicaprojects21 <monicamallampatti21@gmail.com>
2022-10-06 21:55:10 -0400

    first commit

---
+++ gitapi.py
@@ -0,0 +1,37 @@
+"""
+Nihar Shah
+SSW-567: SW Testing, Qual. Assur. & Maint
+Fall 2022
+HW 04a: Develop with the Perspective of the Tester in mind
+"""
+
+import requests
+import json
+from constantentries import GITHUB_URL, USERS, REPOS, SLASH, COMMITS
+
+def githubapi(id):
+    response = requests.get(GITHUB_URL + USERS + SLASH + id + REPOS)
+
+    if response.status_code != 200:
+        print("No Account with this id")
+        return False
+
+    response = response.json()
+
+
+    if len(response) == 0:
+        print("There is no such repository")
+        return False
+    
+
+    for repo in response:
+        repoResponse = requests.get(repo['commits_url'].split("{")[0])
+        repoResponse = repoResponse.json()
+        print("Repository Name: "+ repo['name'] + " \t\t\t\tNumber Of Commits: " + str(len(repoResponse)))
+
+    return True
+
+
+if __name__ == '__main__':
+    userInput = input("Enter Github username: ")
+    githubapi(userInput)
---
+++ testgitapi.py
@@ -0,0 +1,22 @@
+
+
+"""
+Nihar Shah
+SSW-567: SW Testing, Qual. Assur. & Maint
+Fall 2022
+HW 04a: Develop with the Perspective of the Tester in mind
+"""
+
+import unittest
+
+from githubapi import githubapi
+
+class Testgithubapi(unittest.TestCase):
+    def testGithub(self):
+        self.assertEqual(githubapi('?'), False)
+    def testGithub2(self):
+        self.assertEqual(githubapi('NehharShah'), True)
+
+if __name__ == '__main__':
+    print("Test cases are running")
+    unittest.main()
