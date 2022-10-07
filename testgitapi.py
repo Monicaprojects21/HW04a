1d076f48333ac5c68c519591c0a2001af8439bae
Monicaprojects21 <monicamallampatti21@gmail.com>
2022-10-06 22:19:12 -0400

    requests commit

--- testgitapi.py
+++ testgitapi.py
@@ -1,22 +1,19 @@
-
-
-"""
-Nihar Shah
-SSW-567: SW Testing, Qual. Assur. & Maint
-Fall 2022
-HW 04a: Develop with the Perspective of the Tester in mind
-"""
-
 import unittest
 
-from githubapi import githubapi
+from gitapi import githubapi
 
 class Testgithubapi(unittest.TestCase):
     def testGithub(self):
         self.assertEqual(githubapi('?'), False)
     def testGithub2(self):
-        self.assertEqual(githubapi('NehharShah'), True)
+        self.assertEqual(githubapi('Monicaprojects21'), True)
 
 if __name__ == '__main__':
     print("Test cases are running")
     unittest.main()
+    """
+    Monica Malamputti
+SSW-567: SW Testing, Qual. Assur. & Maint
+Fall 2022
+HW 04a: Develop with the Perspective of the Tester in mind
+"""
